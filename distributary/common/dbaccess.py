f5d44b4d3b636044d4ae2320291f3914c4216ec8
Brett Kugler <brett.kugler@gdt.com>
2018-02-08 22:55:22 -0600

    Store workflows.

--- distributary/common/dbaccess.py
+++ distributary/common/dbaccess.py
@@ -4,5 +4,6 @@ from distributary.workflow_gui import app
 
 # TODO: Get this into secrets
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://zyeevjvecouqzr:82a3710c58c9aee4b34edf164aca4d1dc61427b5813ba566d2fd9881e59521d1@ec2-54-83-59-144.compute-1.amazonaws.com:5432/dbpq2vnkm81tr0'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
